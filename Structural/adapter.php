76a979313277df8478dc9943356c7ee61ef14f16
leorus <495325806@qq.com>
2018-08-28 00:21:49 +0800

    Adopter

--- Structural/adapter.php
+++ Structural/adapter.php
@@ -28,4 +28,25 @@ $Ader = new Adapter($Adee);
 $Ader->extendFun1();
 
 
+/**
+ * 第二种方式：类适配器
+ */
+interface Target2 {
+    public function sampleMethod1();
+    public function sampleMethod2();
+}
+
+class Adaptee2 { // 源角色
+    public function sampleMethod1() {echo '++++++++';}
+}
+
+class Adapter2 extends Adaptee2 implements Target2 { // 适配后角色
+    public function sampleMethod2() {echo '————————';}
+}
+
+$adapter = new Adapter2();
+$adapter->sampleMethod1();//输出：++++++++
+$adapter->sampleMethod2();//输出：————————
+
+
 ?>
